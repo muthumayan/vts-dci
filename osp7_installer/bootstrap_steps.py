c4b9716f97f9d04d777a047b30ad437c2e172968
Richard Winters <riwinter@cisco.com>
2016-03-22 14:54:16 -0400

    Update for ASR driver install

--- osp7_installer/bootstrap_steps.py
+++ osp7_installer/bootstrap_steps.py
@@ -4,6 +4,9 @@ __author__ = 'brdemers'
 #
 bootstrap_steps = {
 
+    "asr-deploy": [
+        {"AnsibleStep": "asr_deploy"},
+    ],
     "deploy": [
         # { "ExampleStep": 'example' },
         # { "AnsibleStep": 'rhel_unregister' },
