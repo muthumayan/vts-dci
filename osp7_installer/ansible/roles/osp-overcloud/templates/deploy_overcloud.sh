51924948185e0ab015e3db9161271b7ebeb10449
Sandhya Dasu <sadasu@cisco.com>
2017-10-06 12:06:28 -0400

    Temorariy removing neutron-flat-networks and neutron-public-interface overcloud deploy options from deploy_overcloud.sh. Added new heat-tripleo-template variables to networking-cisco-environment.yaml.

--- osp7_installer/ansible/roles/osp-overcloud/templates/deploy_overcloud.sh
+++ osp7_installer/ansible/roles/osp-overcloud/templates/deploy_overcloud.sh
@@ -13,7 +13,7 @@ openstack overcloud deploy --templates \
   --neutron-network-type vlan \
 #  --neutron-disable-tunneling \
 #  --neutron-flat-networks {{ neutron_flat_networks }} \
-  --neutron-public-interface {{ controller_external_nic }} \
+#  --neutron-public-interface {{ controller_external_nic }} \
 {% if osp_version == 7 %}
   --hypervisor-neutron-public-interface {{ compute_tenant_nic }} \
 {% endif %}
