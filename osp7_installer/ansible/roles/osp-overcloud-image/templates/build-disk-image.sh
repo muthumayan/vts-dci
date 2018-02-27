052e013e1160fb8db905be0079f1c21d510ce3dc
Sandhya Dasu <sadasu@cisco.com>
2018-02-27 12:09:52 -0500

    Another attempt at adding networking-cisco to OSP11 overcloud image.

--- osp7_installer/ansible/roles/osp-overcloud-image/templates/build-disk-image.sh
+++ osp7_installer/ansible/roles/osp-overcloud-image/templates/build-disk-image.sh
@@ -81,9 +81,22 @@ virt-customize --selinux-relabel -a overcloud-full.qcow2 --run-command "patch -f
 #virt-customize -a overcloud-full.qcow2 --run-command 'yum clean all'
 
 # Temp fix to pull down networking-cisco - SD
-virt-customize -a overcloud-full.qcow2 --run-command 'subscription-manager register --user=sdasu --password=CiscoDCI2018'
-virt-customize -a overcloud-full.qcow2 --run-command 'subscription-manager attach --pool=8a85f98c60c5e69f0160c6c9e6da0e81'
-virt-customize -a overcloud-full.qcow2 --run-command 'sudo yum install -y python-networking-cisco'
+#virt-customize -a overcloud-full.qcow2 --run-command 'subscription-manager register --user=sdasu --password=CiscoDCI2018'
+#virt-customize -a overcloud-full.qcow2 --run-command 'subscription-manager attach --pool=8a85f98c60c5e69f0160c6c9e6da0e81'
+#virt-customize -a overcloud-full.qcow2 --run-command 'sudo yum install -y python-networking-cisco'
+
+virt-customize \
+   --memsize 2000 \
+   --add overcloud-full.qcow2 \
+   --sm-credentials "sdasu:password:CiscoDCI2018" \
+   --sm-register \
+   --sm-attach "pool:8a85f98c60c5e69f0160c6c9e6da0e81" \
+   --run-command "subscription-manager repos --disable='*' --enable='rhel-7-server-openstack-11.0-rpms'" \
+   --install python-networking-cisco \
+   --sm-remove \
+   --sm-unregister 
+#   --selinux-relabel 2>&1 | tee -a ~/pilot/customize_image.log
+
 #virt-customize -a overcloud-full.qcow2 --run-command 'cd /tmp/net-cisco'
 #virt-customize -a overcloud-full.qcow2 --run-command 'python setup.py install'
 
