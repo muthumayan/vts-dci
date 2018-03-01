e7fcc91abd66761443bb654cbc395680e53cecfb
Sandhya Dasu <sadasu@cisco.com>
2018-03-01 14:55:02 -0500

    Taking care of deprecated input parametrs to "openstack oveloud deploy"
    These parameters have been deprecated in OSP11.

--- osp7_installer/ansible/roles/osp-overcloud/templates/deploy_overcloud.sh
+++ osp7_installer/ansible/roles/osp-overcloud/templates/deploy_overcloud.sh
@@ -10,7 +10,7 @@ openstack overcloud deploy --templates \
   --ceph-storage-scale {{ overcloud_ceph_storage_scale }} \
   --block-storage-scale {{ overcloud_block_storage_scale }} \
   --swift-storage-scale {{ overcloud_swift_storage_scale }} \
-{% if osp_version < 10 %}
+{% if osp_version < 11 %}
   --neutron-network-type vlan \
   --neutron-network-vlan-ranges {{ network_nexus_vlan_range }} \
   --neutron-flat-networks {{ neutron_flat_networks }} \
@@ -19,7 +19,7 @@ openstack overcloud deploy --templates \
 {% if osp_version == 7 %}
   --hypervisor-neutron-public-interface {{ compute_tenant_nic }} \
 {% endif %}
-{% if osp_version < 10 %}
+{% if osp_version < 11 %}
   --neutron-physical-bridge {{ neutron_tenant_bridge }} \
 {% endif %}
 {% if network_isolation %}
@@ -32,17 +32,17 @@ openstack overcloud deploy --templates \
 {% if deploy_with_flavors %}
   {{ deploy_with_flavors_args }} \
 {% endif %}
-{% if osp_version < 10 %}
+{% if osp_version < 11 %}
   --neutron-tunnel-type vlan \
 {% endif %}
-{% if osp_version < 10 %}
+{% if osp_version < 11 %}
   {% if neutron_external_bridge == neutron_tenant_bridge %}
     --neutron-bridge-mappings datacentre:{{ neutron_tenant_bridge }} \
   {% else %}
     --neutron-bridge-mappings "datacentre:{{ neutron_tenant_bridge }},external:{{ neutron_external_bridge }}" \
   {% endif %}
 {% endif %}
-{% if osp_version < 10 %}
+{% if osp_version < 11 %}
   --neutron-network-vlan-ranges {{ network_nexus_vlan_range }} \
 {% endif %}
 {% if deploy_extra_args %}
