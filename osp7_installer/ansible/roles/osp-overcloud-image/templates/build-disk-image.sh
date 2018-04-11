1501b11b286b45776cf6586009acd83466aa418a
Sandhya Dasu <sadasu@cisco.com>
2018-04-11 18:53:15 -0400

    Undoing previous change which didn't work.

--- osp7_installer/ansible/roles/osp-overcloud-image/templates/build-disk-image.sh
+++ osp7_installer/ansible/roles/osp-overcloud-image/templates/build-disk-image.sh
@@ -88,7 +88,7 @@ correct_selinux_context '/usr/lib/python2.7/site-packages/neutron' '/usr/lib64/p
 correct_selinux_context '/usr/lib/python2.7/site-packages/neutron' '/usr/lib/python2.7/site-packages/UcsSdk*'
 correct_selinux_context '/usr/lib/python2.7/site-packages/neutron' '/usr/share/openstack-puppet'
 
-virt-customize -v -x --selinux-relabel -a overcloud-full.qcow2 --copy-in /usr/share/openstack-puppet/modules/neutron/manifests/plugins/ml2:/usr/share/openstack-puppet/modules/neutron/manifests/plugins/ml2
+#virt-customize -v -x --selinux-relabel -a overcloud-full.qcow2 --copy-in /usr/share/openstack-puppet/modules/neutron/manifests/plugins/ml2:/usr/share/openstack-puppet/modules/neutron/manifests/plugins/ml2
 
 # update 40-hiera-datafiles
 #virt-customize --selinux-relabel -a overcloud-full.qcow2 --upload tripleo-puppet-elements/elements/hiera/os-refresh-config/configure.d/40-hiera-datafiles:/usr/libexec/os-refresh-config/configure.d
