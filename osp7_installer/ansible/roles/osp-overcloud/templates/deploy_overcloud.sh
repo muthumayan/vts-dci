8168e6af7243d56882274b386b3cdf7484184380
Muthumayan <muthu@cisco.com>
2018-05-10 18:07:35 -0700

    customizing to VTS environment for overcloud

--- osp7_installer/ansible/roles/osp-overcloud/templates/deploy_overcloud.sh
+++ osp7_installer/ansible/roles/osp-overcloud/templates/deploy_overcloud.sh
@@ -1,40 +1,15 @@
-#! /bin/bash
-
-source /home/stack/stackrc
-
-cd /home/stack/
-
-openstack overcloud deploy --templates \
-  --control-scale {{ overcloud_control_scale }} \
-  --compute-scale {{ overcloud_compute_scale }} \
-  --ceph-storage-scale {{ overcloud_ceph_storage_scale }} \
-  --block-storage-scale {{ overcloud_block_storage_scale }} \
-  --swift-storage-scale {{ overcloud_swift_storage_scale }} \
-  --neutron-network-type vlan \
-  --neutron-network-vlan-ranges {{ network_nexus_vlan_range }} \
-  --neutron-flat-networks {{ neutron_flat_networks }} \
-  --neutron-public-interface {{ controller_external_nic }} \
-{% if osp_version == 7 %}
-  --hypervisor-neutron-public-interface {{ compute_tenant_nic }} \
-{% endif %}
-  --neutron-physical-bridge {{ neutron_tenant_bridge }} \
-{% if network_isolation %}
-  -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
-{% endif %}
-  -e /home/stack/templates/networking-cisco-environment.yaml \
-{% if nfs_for_storage %}
-  -e /home/stack/templates/nfs-environment.yaml \
-{% endif %}
-{% if deploy_with_flavors %}
-  {{ deploy_with_flavors_args }} \
-{% endif %}
-  --neutron-disable-tunneling \
-{% if neutron_external_bridge == neutron_tenant_bridge %}
-  --neutron-bridge-mappings datacentre:{{ neutron_tenant_bridge }} \
-{% else %}
-{% endif %}
-{% if deploy_extra_args %}
-   {{ deploy_extra_args }} \
-{% endif %}
-  --ntp-server 1.ntp.esl.cisco.com
-
+#!/bin/bash
+openstack overcloud deploy \
+--templates \
+-e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
+-e /home/stack/templates/network-environment.yaml \
+--control-scale 3 \
+--compute-scale 3 \
+--control-flavor control \
+--compute-flavor compute \
+--neutron-network-type vxlan \
+--neutron-tunnel-types vxlan \
+--debug \
+--log-file oclogs/overcloudDeploy_$(date +%m_%d_%y__%H_%M_%S).log \
+--ntp-server ntp.esl.cisco.com \
+--verbose --timeout 100
